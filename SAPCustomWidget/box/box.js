fafe6eae6b10989303f2a52e787edf489256248d
girishmurali2404 <69243058+girishmurali2404@users.noreply.github.com>
2020-08-29 20:40:36 +0530

    Update box.js

--- SAPCustomWidget/box/box.js
+++ SAPCustomWidget/box/box.js
@@ -93,7 +93,7 @@
 			
 			if(rounded >=0 && rounded <=100) {
 				this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 2;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + color + ';}.metric.participation text {fill: ' + color + ';}';
-				this.$svg.innerHTML = '<path d="M150 0 L75 100 L225 100 Z"></path><text class="percentage" text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">' + rounded + '%</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal">' + info + '</text><path d="' + x + '" class="data-arc"></path>"';
+				this.$svg.innerHTML = '<path d="M150 0 L75 100 L225 100 Z"></path><text class="percentage" text-anchor="middle" alignment-baseline="middle" x="150" y="60" font-size="50" font-weight="bold">' + rounded + '%</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal">' + info + '</text><path d="' + x + '" class="data-arc"></path>"';
 				}
 		}
 		  
